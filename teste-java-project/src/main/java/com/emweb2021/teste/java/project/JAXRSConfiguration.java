90214d6f5231524157baf2f8dc793a6818f70460
enio1 <enio1@DESKTOP-Q236M5B>
2021-03-27 19:02:54 -0300

    First commit

---
+++ teste-java-project/src/main/java/com/emweb2021/teste/java/project/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.emweb2021.teste.java.project;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
